057f8d71b093dba0cc4f0cb6536f331e92826d03
GrGLeo <graziani.gothe.leo@gmail.com>
2024-08-28 17:39:17 +0000

    parsing to pandas df

--- back/parsing.py
+++ back/parsing.py
@@ -1,7 +1,21 @@
+import pandas as pd
 from fitparse import FitFile
 
-fitfile = FitFile('run.fit')
 
-for record in fitfile.get_messages('record'):
-    for data in record:
-        print(f'{data.name}: {data.value}')
+def fit_parsing(file: str) -> pd.DataFrame:
+    fitfile = FitFile(file)
+    records = []
+
+    for record in fitfile.get_messages('record'):
+        record_data = {}
+        for data in record:
+            record_data[data.name] = data.value
+        records.append(record_data)
+
+    df = pd.DataFrame(records)
+    return df
+
+
+df = fit_parsing('run.fit')
+print(df.tail())
+print(df.columns)
