26fc561e33d555c740087463b6b063744409bb71
GrGLeo <graziani.gothe.leo@gmail.com>
2024-08-31 08:15:01 +0000

    formatting

--- front/pages/2_analytics.py
+++ front/pages/2_analytics.py
@@ -2,7 +2,12 @@ import datetime
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
+
+
+def time_to_seconds(t):
+    h, m, s = t.hour, t.minute, t.second
+    time = datetime.timedelta(hours=h, minutes=m, seconds=s).total_seconds()
+    return int(time)
 
 
 st.title("Workout analystics")
@@ -11,13 +16,6 @@ db_engine = st.session_state.db_engine
 
 df = pd.read_sql("SELECT * FROM lap_running", db_engine)
 st.write(df)
-st.write(df.info())
-
-
-def time_to_seconds(t):
-    h, m, s = t.hour, t.minute, t.second
-    return int(datetime.timedelta(hours=h, minutes=m, seconds=s).total_seconds())
-
 
 df['time_seconds'] = df['timer'].apply(time_to_seconds)
 
@@ -27,9 +25,8 @@ df['end_seconds'] = df['start_seconds'] + df['time_seconds']
 df['start_time'] = pd.to_datetime(df['start_seconds'], unit='s')
 df['end_time'] = pd.to_datetime(df['end_seconds'], unit='s')
 
-fig = px.timeline(df, x_start='start_time', x_end='end_time', y='pace', color='pace', 
-                  labels={'start_time': 'Start Time', 'end_time': 'End Time', 'pace': 'Pace'},
-                  title='Pace per interval')
+fig = px.timeline(df, x_start='start_time', x_end='end_time', y='pace',
+                  labels={'start_time': 'Start Time', 'end_time': 'End Time'})
 
 # Update layout
 fig.update_layout(
@@ -41,12 +38,11 @@ fig.update_layout(
     ),
     xaxis=dict(
         tickformat='%H:%M:%S',
-        dtick='3600000',
+        dtick='360000',
         showgrid=True,
         zeroline=True
     )
 )
-
-fig.update_yaxes(range=[0, df['pace'].max() + 1])
+fig.update_yaxes(range=[2, df['pace'].max() + 1])
 
 st.plotly_chart(fig)
--- front/pages/3_upload.py
+++ front/pages/3_upload.py
@@ -1,21 +1,23 @@
 import requests
 import streamlit as st
-import pandas as pd
 
 
 st.title("Fit File Upload")
 
 uploaded_file = st.file_uploader("Choose a file", type=['fit', 'fits'])
 
+app_type = ["application/fit", "application/fits"]
+
 if uploaded_file is not None:
-    if uploaded_file.type == "application/fit" or uploaded_file.type == "application/fits":
+    if uploaded_file.type in app_type:
         with st.spinner("Uploading file..."):
             file = {"file": uploaded_file.getvalue()}
-            response = requests.post("http://127.0.0.1:8000/uploadfile/", files=file)
+            response = requests.post(
+                "http://127.0.0.1:8000/uploadfile/",
+                files=file
+            )
             if response.status_code == 200:
-                df = pd.DataFrame(response.json()["data"])
-                st.session_state['activity'] = df
-                st.write(df.columns)
+                st.write("Upload completed")
             else:
                 st.write("error")
     else:
