302b1d79a7b15f9c6cc38b536607ae4b1021738a
GrGLeo <graziani.gothe.leo@gmail.com>
2024-09-13 15:57:47 +0000

    user class boiler plate

---
+++ front/user/user.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def _get_run_workout(self):
+        pass
+
+    def _get_swim_workout(self):
+        pass
+
+    def _get_cycling_workout(self):
+        pass
+
+    def _get_workout(self):
+        pass
+
+    def get_threshold(self):
+        pass
