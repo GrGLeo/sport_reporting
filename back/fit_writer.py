6d528d8c0db6f99e411960f46ad21273de4f0ffd
GrGLeo <graziani.gothe.leo@gmail.com>
2024-10-17 16:28:05 +0000

    testing writing .fit

---
+++ back/fit_writer.py
@@ -0,0 +1,10 @@
+import datetime
+
+from fit_tool.fit_file_builder import FitFileBuilder
+from fit_tool.profile.messages.file_id_message import FileIdMessage
+from fit_tool.profile.messages.workout_message import WorkoutMessage
+from fit_tool.profile.messages.workout_step_message import WorkoutStepMessage
+from fit_tool.profile.profile_type import Sport, Intensity, WorkoutStepDuration, WorkoutStepTarget, Manufacturer, FileType
+
+
+
