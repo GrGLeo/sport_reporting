4c463fd2e59c200268156cb892cb818f299bced1
GrGLeo <graziani.gothe.leo@gmail.com>
2024-10-25 21:01:27 +0000

    fix setup.py

---
+++ back/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sport_board_back",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Leo Graziani",
+    description="Backend for Sport dashboard",
+    url="https://github.com/GrGLeo/sport_reporting",
+    python_requires='>=3.12'
+)
---
+++ front/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="front",
+    version="0.1.2",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Leo Graziani",
+    description="Front for Sport dashboard",
+    url="https://github.com/GrGLeo/sport_reporting/front",
+    python_requires='>=3.12'
+)
