f44a41c2d502f51a0f43114a5d1ab8aaa713d788
GrGLeo <graziani.gothe.leo@gmail.com>
2024-11-26 23:14:38 +0100

    change constrain

--- migration/002_comments.sql
+++ migration/002_comments.sql
@@ -1,5 +1,5 @@
 -- +goose UP
-CREATE SCHEMA param;
+CREATE SCHEMA IF NOT EXISTS param;
 
 CREATE TABLE param.activity_comments (
   comment_id SERIAL PRIMARY KEY,
@@ -7,7 +7,7 @@ CREATE TABLE param.activity_comments (
   user_id UUID NOT NULL,
   comment TEXT NOT NULL DEFAULT '',
   -- CONSTRAINT fk_activity FOREIGN KEY (activity_id) REFERENCES activities(activity_id) ON DELETE CASCADE,
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES settings.users(user_id) ON DELETE CASCADE
 );
 
 -- +goose Down
