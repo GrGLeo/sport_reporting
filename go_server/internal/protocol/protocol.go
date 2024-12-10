c0cfc616724c47dc0d8de845f638a0e04eb2a1a3
GrGLeo <graziani.gothe.leo@gmail.com>
2024-12-10 18:16:47 +0100

    test: prepare init packet

--- go_server/internal/protocol/protocol.go
+++ go_server/internal/protocol/protocol.go
@@ -1,6 +1,9 @@
 package protocol
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"hash/crc32"
 	"io"
 
@@ -11,6 +14,8 @@ const (
   ProtocolVersion = 1
   MaxPacketSize = 1024
   ServerAddress = "localhost:8090"
+  MessageInit = 1
+  MessagePacket = 2
 )
 
 type InitPacket struct {
@@ -29,15 +34,40 @@ type Header struct {
 }
 
 type FileSender struct {
-  File []byte
+  File io.Reader
   userID uuid.UUID
   fileSize int
+  transactionID int
 }
 
-func (fs *FileSender) SendInitPacket () {
+func (fs *FileSender) SendInitPacket () ([]byte, error) {
+  checksum, err := fs.CalculateChecksum(fs.File)
+  if err != nil {
+    return []byte{}, errors.New("Failed to calculate checksum")
+  }
+
+  initPacket := InitPacket{
+    MessageType: MessageInit,
+    TransactionID: uint16(fs.transactionID),
+    FileSize: uint32(fs.fileSize),
+    Checksum: checksum,
+    UUID: fs.userID,
+  }
+
+  buff := new(bytes.Buffer)
+  err = binary.Write(buff, binary.BigEndian, initPacket)
+  if err != nil {
+    return []byte{}, errors.New("Failed to encode initPacket")
+  }
+    
+  initPacketBytes := buff.Bytes()
+  return initPacketBytes, nil
 }
 
 func (fs *FileSender) SendPacket () {
+}
+
+func (fs *FileSender) SendFile () {
 }
 
 func (fs *FileSender) CalculateChecksum(reader io.Reader) (uint32, error) {
--- go_server/internal/protocol/protocol_test.go
+++ go_server/internal/protocol/protocol_test.go
@@ -1,8 +1,13 @@
 package protocol
 
 import (
+	"bytes"
+	"encoding/binary"
 	"hash/crc32"
 	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculateChecksum(t *testing.T) {
@@ -23,4 +28,40 @@ func TestCalculateChecksum(t *testing.T) {
 		t.Errorf("Checksum mismatch: got %v, want %v", checksum, expectedChecksum)
 	}
 
+}
+
+func TestSendInitPacket(t *testing.T) {
+	data := []byte("example file content")
+  reader := bytes.NewReader(data)
+	userID := uuid.New()
+	transactionID := 1234
+	fileSize := len(data)
+
+	fileSender := &FileSender{
+		File:          reader,
+		userID:        userID,
+		transactionID: transactionID,
+		fileSize:      fileSize,
+	}
+
+	expectedChecksum := crc32.ChecksumIEEE(data)
+
+	expectedInitPacket := InitPacket{
+		MessageType:    MessageInit,
+		TransactionID:  uint16(transactionID),
+		FileSize:       uint32(fileSize),
+		Checksum:       expectedChecksum,
+		UUID:           userID,
+	}
+
+	result, err := fileSender.SendInitPacket()
+
+	assert.NoError(t, err, "Expected no error from SendInitPacket")
+
+	buff := bytes.NewReader(result)
+	var actualInitPacket InitPacket
+	err = binary.Read(buff, binary.BigEndian, &actualInitPacket)
+
+	assert.NoError(t, err, "Failed to decode InitPacket")
+	assert.Equal(t, expectedInitPacket, actualInitPacket, "InitPacket mismatch")
 }
